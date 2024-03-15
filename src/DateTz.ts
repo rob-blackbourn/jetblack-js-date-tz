75be581eb4e9a9ea0381220ee424e22d634fe5e0
Rob Blackbourn <rob.blackbourn@gmail.com>
2024-03-15 14:20:28 +0000

    Made DateTz fields private

--- src/DateTz.ts
+++ src/DateTz.ts
@@ -3,8 +3,8 @@ import { Timezone } from './Timezone'
 import { tzUtc } from './UTCTimezone'
 
 export class DateTz {
-  public date: Date
-  public tz: Timezone
+  private _date: Date
+  private _tz: Timezone
 
   constructor()
   constructor(value: number)
@@ -53,43 +53,43 @@ export class DateTz {
   )
   constructor(...args: any[]) {
     if (args.length === 0) {
-      this.date = new Date()
-      this.tz = tzLocal
+      this._date = new Date()
+      this._tz = tzLocal
     } else if (args.length == 1) {
       if (args[0] instanceof DateTz) {
-        this.tz = args[0].tz
-        this.date = new Date(args[0].date)
+        this._tz = args[0].tz
+        this._date = new Date(args[0].date)
       } else if (args[0] instanceof Timezone) {
-        this.tz = args[0]
-        this.date = new Date()
+        this._tz = args[0]
+        this._date = new Date()
       } else if (typeof args[0] === 'string') {
         // Date only strings are utc
         if (args[0].length <= 'YYYY-MM-DD'.length) {
-          this.tz = tzUtc
-          this.date = new Date(args[0])
+          this._tz = tzUtc
+          this._date = new Date(args[0])
         } else {
-          this.tz = tzLocal
-          this.date = new Date(args[0])
+          this._tz = tzLocal
+          this._date = new Date(args[0])
         }
       } else if (typeof args[0] === 'number' || args[0] instanceof Date) {
-        this.tz = tzLocal
-        this.date = new Date(args[0])
+        this._tz = tzLocal
+        this._date = new Date(args[0])
       } else {
         throw new RangeError('Invalid date')
       }
     } else if (args.length === 2 && args[1] instanceof Timezone) {
-      this.tz = args[1]
+      this._tz = args[1]
 
       if (typeof args[0] === 'string') {
         if (args[0].length <= 'YYYY-MM-DD'.length) {
-          this.date = tzUtc.as(new Date(args[0]), this.tz)
+          this._date = tzUtc.as(new Date(args[0]), this.tz)
         } else {
-          this.date = tzLocal.as(new Date(args[0]), this.tz)
+          this._date = tzLocal.as(new Date(args[0]), this.tz)
         }
       } else if (typeof args[0] === 'number') {
-        this.date = new Date(args[0])
+        this._date = new Date(args[0])
       } else if (args[0] instanceof Date) {
-        this.date = new Date(args[0])
+        this._date = new Date(args[0])
       } else {
         throw new RangeError('Invalid date')
       }
@@ -99,16 +99,16 @@ export class DateTz {
         args[args.length - 1] instanceof Timezone &&
         args.slice(0, args.length - 1).every(x => typeof x === 'number')
       ) {
-        this.tz = args[args.length - 1]
+        this._tz = args[args.length - 1]
         args = args.slice(0, args.length - 1)
       } else if (args.length <= 7 && args.every(x => typeof x === 'number')) {
-        this.tz = tzLocal
+        this._tz = tzLocal
       } else {
         throw new RangeError('Invalid args')
       }
       const [year, month, day, hours, minutes, seconds, milliseconds] =
         args as number[]
-      this.date = this.tz.makeDate(
+      this._date = this.tz.makeDate(
         year,
         month,
         day,
@@ -118,6 +118,13 @@ export class DateTz {
         milliseconds
       )
     }
+  }
+
+  get date() {
+    return this._date
+  }
+  get tz() {
+    return this._tz
   }
 
   get year() {
