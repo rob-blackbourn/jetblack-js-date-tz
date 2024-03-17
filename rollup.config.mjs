7642ce7d4ca81f55503cd7d73135043bae059b95
Rob Blackbourn <rob.blackbourn@gmail.com>
2024-03-17 09:42:20 +0000

    Alpha version bump

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,20 +1,41 @@
 import typescript from '@rollup/plugin-typescript'
+import terser from '@rollup/plugin-terser'
+import { globSync } from 'glob'
 
+const sources = globSync('src/*.ts', {
+  ignore: ['src/utils.ts', 'src/type.ts']
+})
 const input = 'src/index.ts'
 const output = { sourcemap: true }
 const outputDir = 'dist'
-const config = ['es', 'cjs'].map(format => ({
-  input,
-  output: {
-    ...output,
-    dir: `${outputDir}/${format}`,
-    format
+const config = ['es', 'cjs'].flatMap(format => [
+  {
+    input: sources,
+    output: {
+      ...output,
+      dir: `${outputDir}/${format}`,
+      format
+    },
+    plugins: [
+      typescript({
+        declarationDir: `${outputDir}/${format}`
+      })
+    ]
   },
-  plugins: [
-    typescript({
-      declarationDir: `${outputDir}/${format}`
-    })
-  ]
-}))
+  {
+    input: sources,
+    output: {
+      ...output,
+      dir: `${outputDir}/min/${format}`,
+      format,
+      plugins: [terser()]
+    },
+    plugins: [
+      typescript({
+        declarationDir: `${outputDir}/min/${format}`
+      })
+    ]
+  }
+])
 
 export default config
